a92a0fcd5a4a7201dace53e622dd36dd25873073
sreeharsha-alluri <53439217+sreeharsha-alluri@users.noreply.github.com>
2022-08-20 14:01:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World, this is a first change'));
+app.get('/', (req, res) => res.send('This is a second change in the code.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
